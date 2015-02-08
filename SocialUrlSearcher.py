002dd1dd68222aca6a6aa5807311e6f2240dd06d
Jacob Dubinsky <dubinskyjm@gmail.com>
2015-02-07 19:07:10 -0700

    updated to search social mention for liveleak title - using flask as a web service wrapper to play with flask!

---
+++ SocialUrlSearcher.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+from flask import Flask
+import requests
+import json
+
+app = Flask(__name__)
+
+@app.route("/liveleak/<title>")
+def social_search(title):
+    r = _social_search_helper(title)
+    comment = None
+    for i in range(len(r['items'])):
+        comment = r['items'][0]['description'].strip()
+        print(comment)
+        if len(comment) > 0:
+            break
+
+    if comment is None or len(comment) == 0:
+        comment = "Shit! Jacob Bot didn't find anything about {0}".format(title)
+
+    return comment
+
+def _social_search_helper(title):
+    BASE_URL = "http://socialmention.com/search?q="
+    URI_EXTENSIONS = "&f=json&t=comments&src=facebook"
+    r = requests.get(BASE_URL + title + URI_EXTENSIONS)
+    return json.loads(r.text)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host="0.0.0.0")
+
+
+
--- liveleakDetector.py
+++ liveleakDetector.py
@@ -1,32 +1,70 @@
-import xmpp
+#!/usr/bin/python2
 
-liveleakmessage = "Hello, this is Andrew Bot, I noticed that you have sent me a liveleak link. I just wanted to let you know that there is a 5% chance I will view this link"
+# python2 because xmpp not working in python3...
+
+import xmpp
+import requests
+from BeautifulSoup import BeautifulSoup
+import urllib
+import string
+
+SOCIAL_SEARCH_URL = "http://127.0.0.1:5000/liveleak/"
+FB_CHAT_URL = "chat.facebook.com"
+CREDENTIALS_FILE = "credentials"
+
+credentials_file = open(CREDENTIALS_FILE, 'r')
+creds = credentials_file.readline()
+username, passwd = creds.strip().split(':')
+
+jid = xmpp.protocol.JID(username)
+client = xmpp.Client(jid.getDomain(), debug=[])
+
+def sanitize(s):
+        s = filter(lambda x: x in string.printable, s)
+        return urllib.quote(s)
 
 def messageCB(sess,mess):
     nick=mess.getFrom().getResource()
     text=mess.getBody()
     fromUser = mess.getFrom()
-    
+
     if text is not None:
         if "liveleak.com" in text:
-            
-            client = xmpp.Client('facebook.com',debug=[])
+            text_list = text.split()
+            liveleak_link = None
+            for t in text_list:
+                    if "liveleak.com" in t:
+                            liveleak_link = t
+            if liveleak_link is None:
+                    print "no link found"
+                    return
+
+            title = get_live_leak_title(liveleak_link)
+            title = sanitize(title)
+            r = requests.get(SOCIAL_SEARCH_URL + title)
+            client = xmpp.Client(jid.getDomain(), debug=[])
             client.connect(server=('chat.facebook.com',5222))
-            client.auth(username, passwd)
+            client.auth(jid.getNode(), passwd)
             client.sendInitPresence()
-            message = xmpp.Message(fromUser, liveleakmessage)
+            message = xmpp.Message(fromUser, r.text)
             message.setAttr('type', 'chat')
             client.send(message)
         
         else:
-            print "no liveleak"
+            print("no liveleak")
 
-username = '##########PUT USERNAME HERE$############'
-passwd = '##########PUT PASSWORD HERE$############'
 
-client = xmpp.Client('facebook.com',debug=[])
+def get_live_leak_title(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text)
+    title = soup.html.head.title.string
+    if "LiveLeak.com" in title:
+            return title.split("LiveLeak.com - ")[1]
+    else:
+            return title
+
 client.connect(server=('chat.facebook.com',5222))
-client.auth(username, passwd)
+client.auth(jid.getNode(), passwd)
 client.sendInitPresence()
 client.RegisterHandler('message',messageCB)
 
